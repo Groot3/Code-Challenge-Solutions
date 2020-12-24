5e15e983bb0c58ea34beb5d106816029ebd5019c
Joe H <stik0174@gmail.com>
2020-12-24 01:25:00 -0500

    color detect v.01
    work in progress

---
+++ Dec 2020/ColorDetector/cdetect.js
@@ -0,0 +1,14 @@
+const robot = require("robotjs");
+
+const mousepos = robot.getMousePos()
+const screensize = robot.getScreenSize()
+
+// gets the pixel color from mouse position
+
+// if (/*TODO: implement check for key combination*/){
+//     mousepixelcolor = robot.getPixelColor(mousepos)
+// }
+
+// if (/*TODO: implement check for key combination*/){
+
+// }
