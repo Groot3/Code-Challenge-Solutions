3f27fae7fba9f627612da0c428fa3bec4fb26dd3
Joe H <stik0174@gmail.com>
2021-01-08 20:10:18 -0500

    refactor / description WIP functionality
    refactor code to give a brief description of the article. broke lots of things, added lots of things. Promises are difficult. Functionality for the description is currently locked to console.log. 
    Next up: resolving in the promise which contains the description! Almost there.

--- Jan 2021/scp.js
+++ Jan 2021/scp.js
@@ -2,57 +2,74 @@
 //1/6/2021
 
 const keys = require('./config.json')
-
 const discord = require('discord.js')
 const client = new discord.Client()
+const cmds = ["!scp"]
+const SCPscrape = require('./scrape.js')
 
-const commands = ["!scp"]
 
 client.once('ready', () => {
     console.log("Ready.")
 })
-client.on('message', message => {
 
-    // numeric function from StackOverflow to verify input
-    function isNumeric(n) {
-        return !isNaN(parseFloat(n)) && isFinite(n);
-    }
+try {
+    client.on('message', msg => {
 
-    generateRandom = function(toNum) {
-        generateRandom = Math.floor(Math.random() * toNum) + 1;
-        return generateRandom
-    };
+        // numeric function from StackOverflow to verify input
+        function isNumeric(n) {
+            return !isNaN(parseFloat(n)) && isFinite(n);
+        }
+    
+        generateRandom = function(toNum) {
+            generateRandom = Math.floor(Math.random() * toNum) + 1;
+            return generateRandom
+        };
 
-    RandomSCP = generateRandom(4999)
-    if (message.author.bot == true) {
-        return
-    }
+        RandomSCP = generateRandom(4999)
+        if (msg.author.bot == true) {
+            return
+        }
+    
+        for (cmd of cmds) {
+            if (msg.content.includes(cmd) && msg.content != (cmd)) {
+                ReqSCP = msg.content
+                ReqSCP = ReqSCP.replace(`${cmd}`, "").replace(" ","") 
+                // formats spaces and the given command out of string
+    
+                if (isNumeric(ReqSCP) == true) {
+                    // Checks to see if the formatted string is a number
+                    if (ReqSCP < 5000) {
+                        if (ReqSCP < 100) {
+                            msg.channel.send(`http://www.scpwiki.com/scp-0${ReqSCP}`) 
+                            // fix for:SCP links must be formatted ending in -XXX, not -XX.
 
-    for (command of commands) {
-        if (message.content.includes(command) && message.content != (command)) {
-            RequestedSCP = message.content
-            RequestedSCP = RequestedSCP.replace(`${command}`, "").replace(" ","") 
-            // formats spaces and command out of string
-
-            if (isNumeric(RequestedSCP) == true) {
-                // Checks to see if the formatted string is a number
-                if (RequestedSCP < 5000) {
-                    message.channel.send(`http://www.scpwiki.com/scp-${RequestedSCP}`)
-                } else 
-                {
-                    message.channel.send(`Error: try a SCP within range 1-4999 instead.`)
+                        } else {
+                            msg.channel.send(`http://www.scpwiki.com/scp-${ReqSCP}`)
+                        }
+                    } else 
+                    {
+                        msg.channel.send(`Error: try a SCP within range 1-4999 instead.`)
+                    }
+                } else {
+                    msg.channel.send(`Error: Command not recognized. Try entering a number.`)
+                    // if ReqSCP is not numeric.
                 }
-            } else {
-                message.channel.send(`Error: Command not recognized. Try entering a number.`)
+                
             }
-            
-        }
-        if (message.content === (command)) {
-            message.channel.send(`http://www.scpwiki.com/scp-${RandomSCP}`)
-        }
-    }
-    return
-})
 
+            let desc = ''
+            if (msg.content === (cmd)) { // if no condition is given:
+                async function getdescription() {
+                    await SCPscrape(`http://www.scpwiki.com/scp-${RandomSCP}`)
+                }
+                
+                msg.channel.send(`http://www.scpwiki.com/scp-${RandomSCP}` + "\n" + getdescription())
+            }
+        }
+    })
+} catch (error) {
+    console.error(error)
+}
+//Makes sure bot doesnt die on one error
 
 client.login(`${keys.BOT_TOKEN}`)
